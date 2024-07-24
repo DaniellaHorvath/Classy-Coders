3a18e20fffb425c69f1fad97581f5d8d8ac02976
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-24 16:11:54 +0100

    Completed the SoftwareEngineer task

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,10 +1,32 @@
-class SoftwareEngineer {
+const { Employees } = require("./Employees");
 
+class SoftwareEngineer extends Employees {
+  #programmingLanguages = [];
 
+  constructor(name, position, salary, programmingLanguages) {
+    super(name, position, salary);
+    this.#programmingLanguages = programmingLanguages;
+  }
 
-    
+  getProgrammingLanguages() {
+    return this.#programmingLanguages;
+  }
+
+  setProgrammingLanguages(language) {
+    this.#programmingLanguages.push(language);
+  }
 }
+
+const programmer = new SoftwareEngineer(
+  "Becca",
+  "Senior Software Engineer",
+  100000,
+  ["JavaScript", "Java", "Python"]
+);
+console.log(programmer.getProgrammingLanguages()); // ["JavaScript", "Java", "Python"]
+programmer.setProgrammingLanguages("C#");
+console.log(programmer.getProgrammingLanguages()); // ["JavaScript", "Java", "Python", "C#"]
 
 module.exports = {
-    SoftwareEngineer,
-}
+  SoftwareEngineer,
+};
