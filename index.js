75d6df17b07aa666d43fe25b6223d24ec7659ca9
HannahS62 <hannah_smith6@msn.com>
2024-07-24 14:35:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Employees } = require("./classes/Employees")
+const { Manager } = require("./classes/Manager")
+const { SalesPerson } = require("./classes/SalesPerson")
+const { SoftwareEngineer } = require("./classes/SoftwareEngineer")
+
+// Run and debug your code here
+
+
+
+
+
+
+module.exports = {
+    Employees,
+    Manager,
+    SalesPerson,
+    SoftwareEngineer
+}
